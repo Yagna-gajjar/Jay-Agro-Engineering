d354db8406636d00904898f6b28e3d26f98299d1
Yagna-gajjar <yagnagangajaliya@gmail.com>
2024-06-06 10:22:44 +0530

    updated

--- jayagro/src/Navbar.js
+++ jayagro/src/Navbar.js
@@ -69,46 +69,79 @@ export default function Navbar() {
     return (
         <>
             <NavContact />
-            <nav class="navbar navbar-expand-xl bg-warning p-0 p-md-4">
+            <nav class="navbar navbar-expand-xl bg-warning p-0 p-md-4 d-flex justify-content-between">
                 <div class="container-fluid">
-                    <div>
-                        <img className="logoimg me-1" src={Img} />
-                        <Link class="navbar-brand text-white ps-1 ps-lg-4 text-truncate" to="/">JAY AGRO ENGINEERING.</Link>
-                    </div>
-                    <div>
-                        <div>
-                            <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
-                                <li class="nav-item mb-1">
-                                    <Link class="nav-link active text-truncate" aria-current="page" to="/">Home</Link>
-                                </li>
-                                <li class="nav-item mb-1">
-                                    <Link to="./About" class="nav-link text-truncate">About Us</Link>
-                                </li>
-                                <li class="nav-item dropdown mb-1">
-                                    <div class="btn-group dropnavlink">
-                                        <Link to="./DTHrig" class="nav-link text-truncate">DTH Rig
-                                        </Link>
-                                        <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </button>
-                                        <ul class="dropdown-menu">{a}
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown mb-1">
-                                    <div class="btn-group dropnavlink">
-                                        <Link to="./ProductPart" class="nav-link text-truncate">Product Part</Link>
-                                        <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            {b}
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <Link to="./contact" class="nav-link text-truncate mb-1 ps-1 pe-1">Contact Us</Link>
-                                </li>
-                            </ul>
+                    <div className="d-flex justify-content-between header_menu_bar">
+                        <div className="d-flex">
+                            <img className="logoimg me-1" src={Img} />
+                            <Link class="navbar-brand text-white ps-1 ps-lg-4 text-truncate" to="/">JAY AGRO ENGINEERING.</Link>
                         </div>
+                        <button class="navbar-toggler" type="button" onClick={() => {
+                            document.getElementsByClassName("phone_menu")[0].classList.toggle("display");
+                        }}>
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="phone_menu">
+                        <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
+                            <li class="nav-item mb-1">
+                                <Link class="nav-link active text-truncate" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li class="nav-item mb-1">
+                                <Link to="./About" class="nav-link text-truncate">About Us</Link>
+                            </li>
+                            <li class="nav-item dropdown mb-1">
+                                <div class="btn-group dropnavlink">
+                                    <Link to="./DTHrig" class="nav-link text-truncate">DTH Rig</Link>
+                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                    <ul class="dropdown-menu">{a}</ul>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown mb-1">
+                                <div class="btn-group dropnavlink">
+                                    <Link to="./ProductPart" class="nav-link text-truncate">Product Part</Link>
+                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                    <ul class="dropdown-menu">{b}</ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="./contact" class="nav-link text-truncate mb-1">Contact Us</Link>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <div className="navlinks">
+                        <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
+                            <li class="nav-item mb-1">
+                                <Link class="nav-link active text-truncate" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li class="nav-item mb-1">
+                                <Link to="./About" class="nav-link text-truncate">About Us</Link>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <div class="btn-group dropnavlink">
+                                    <Link to="./DTHrig" class="nav-link text-truncate">DTH Rig
+                                    </Link>
+                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                                    </button>
+                                    <ul class="dropdown-menu">{a}
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <div class="btn-group dropnavlink">
+                                    <Link to="./ProductPart" class="nav-link text-truncate">Product Part</Link>
+                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        {b}
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="./contact" class="nav-link text-truncate mb-1 ps-1 pe-1">Contact Us</Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 {showModel && <Mymodal />}
