0b71508ef0dc04e241d2de0bf1e2670196deb607
Yagna-gajjar <yagnagangajaliya@gmail.com>
2024-06-09 13:04:02 +0530

    updated

--- jayagro/src/Footer.js
+++ jayagro/src/Footer.js
@@ -1,57 +1,103 @@
-import React, { useState, useReducer } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import toast from "react-hot-toast";
+import "./footer.css";
 
 export default function Footer() {
+    const [dth, setDth] = useState([]);
 
-    const subscribeusers = {
-        Email: ""
-    }
-
-    const reducer = (state, action) => {
-        if (action.type === "merge") {
-            return ({ ...state, [action.prop1]: action.prop2 });
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('https://jay-agro-server.onrender.com/dthrig');
+            setDth(response.data);
         }
-        return state;
-    }
+        fetchData();
+    }, [])
 
-    const [state, dispatch] = useReducer(reducer, subscribeusers);
-    const nav = useNavigate();
+    const [productpart, setProductpart] = useState([]);
 
-    const inputHandler = (e) => {
-        const { name, value } = e.target;
-        dispatch({ type: "merge", prop1: name, prop2: value });
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('https://jay-agro-server.onrender.com/productparts');
+            setProductpart(response.data);
+        }
+        fetchData();
+    }, [])
 
-    const submitForm = async (e) => {
-        e.preventDefault();
+    let a = dth.map((e) => {
+        return (
+            <li><Link class="text-truncate links" to={"/dthdetails/" + e._id}>{e.dthname} DTH Rig</Link></li>
+        );
+    })
 
-        await axios.post('https://jay-agro-server.onrender.com/subscribe', state)
-            .then((response) => {
-                toast.success(response.data.msg, { position: "top-center" });
-                const localemail = localStorage.setItem("subscriber", state.Email);
-                nav("/");
-            }).catch(error => console.log(error.response))
-    }
+    let b = productpart.map((e) => {
+        return (
+            <li><Link class="text-truncate links" to='./ProductPart' >{e.productname}</Link></li>
+        );
+    })
 
-
-    return (<>
-        <div class="footer card text-center m-2">
-            <div class="card-header bg-warning text-white p-2">
-                Subscribe
+    return (<div >
+        <div className="footer">
+            <div className="footer_about p-3 p-md-5">
+                <div className="footer_brand p-md-5">
+                    <img className="img-fluid logoimg" src="https://firebasestorage.googleapis.com/v0/b/jayagro-e8608.appspot.com/o/LOGO.PNG?alt=media&token=d7932cef-1cbd-438a-88b9-8a355d0c5f2b" />
+                    <h1 className="footer_name">Jay Agro</h1>
+                </div>
+                <div className="footer_par">
+                    Jay Agro Engineering is one of the well-known names in the DTH rig machine manufacturing industry. We are a well-established company also manufacturing DTH rig machine parts. We are committed in our efforts towards continuously trying to innovate and develop our machines to be of even more superior quality than what it was previously
+                </div>
             </div>
-            <div class="card-body text-warning">
-                <h5 class="card-title pt-4">Jay Agro Engineering</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <form className="p-4" action="" method="post" onSubmit={submitForm}>
-                    <input className="subemail" type="email" name="Email" onChange={inputHandler} placeholder="Enter your Email" />
-                    <button className="btn btn-warning text-truncate ms-2 rounded-3" type="submit">Subscribe</button>
-                </form>
-            </div>
-            <div class="card-footer text-muted bg-warning">
+            <div className="row">
+                <div className="footer_links col-md-12 col-lg-4 p-3 p-md-5">
+                    <ul>
+                        <h3>Quick Links</h3>
+                        <li><Link className="links" to="/">Home</Link></li>
+                        <li><Link className="links" to="./About">About Us</Link></li>
+                        <li><Link className="links" to="./DTHrig">DTH Rig</Link></li>
+                        <li><Link className="links" to="./ProductPart">Product Part</Link></li>
+                    </ul>
+                </div>
+                <div className="footer_links col-md-12 col-lg-4 p-3 p-md-5">
+                    <ul>
+                        <h3>Our Products & DTH</h3>
+                        {a}
+                        {b}
+                    </ul>
+                </div>
+                <div className="footer_links col-md-12 col-lg-4 p-3 p-md-5">
+                    <ul>
+                        <h3>Contact Us</h3>
+                        <li>
+                            <div className="icon-box links">
+                                <i className="bi bi-geo-alt-fill pe-2"></i>
+                                Metoda, GIDC, Rajkot
+                            </div>
+                        </li>
+                        <li className="links">
+                            GST No.: 24AHJPG7548K1Z0
+                        </li>
+                        <li>
+                            <div className="icon-box">
+                                <i class="bi bi-telephone pe-2"></i>
+                                <a href="tel:+919978422542">9978422542</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="icon-box">
+                                <i class="bi bi-telephone pe-2"></i>
+                                <a href="tel:+919825536118">9825536118</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="icon-box d-flex">
+                                <i class="bi bi-envelope pe-2"></i>
+                                <a href="mailto:jayagroengineerimg@gmail.com">jayagroengineerimg@gmail.com</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </>
+    </div>
     );
 }
--- jayagro/src/Navbar.js
+++ jayagro/src/Navbar.js
@@ -69,47 +69,17 @@ export default function Navbar() {
     return (
         <>
             <NavContact />
-            <nav class="navbar navbar-expand-xl bg-warning p-0 p-md-4 d-flex justify-content-between">
-                <div class="container-fluid">
-                    <div className="d-flex justify-content-between header_menu_bar">
-                        <div className="d-flex">
-                            <img className="logoimg me-1" src={Img} />
-                            <Link class="navbar-brand text-white ps-1 ps-lg-4 text-truncate" to="/">JAY AGRO ENGINEERING.</Link>
-                        </div>
-                        <button class="navbar-toggler" type="button" onClick={() => {
-                            document.getElementsByClassName("phone_menu")[0].classList.toggle("display");
-                        }}>
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+            <nav class="navbar navbar-expand-xl bg-warning p-0 p-md-4">
+                <div className="header_menu_bar">
+                    <div className="d-flex">
+                        <img className="logoimg me-1" src={Img} />
+                        <Link class="navbar-brand text-white ps-1 ps-lg-4 text-truncate" to="/">JAY AGRO ENGINEERING.</Link>
                     </div>
-                    <div className="phone_menu">
-                        <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
-                            <li class="nav-item mb-1">
-                                <Link class="nav-link active text-truncate" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li class="nav-item mb-1">
-                                <Link to="./About" class="nav-link text-truncate">About Us</Link>
-                            </li>
-                            <li class="nav-item dropdown mb-1">
-                                <div class="btn-group dropnavlink">
-                                    <Link to="./DTHrig" class="nav-link text-truncate">DTH Rig</Link>
-                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                                    <ul class="dropdown-menu">{a}</ul>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown mb-1">
-                                <div class="btn-group dropnavlink">
-                                    <Link to="./ProductPart" class="nav-link text-truncate">Product Part</Link>
-                                    <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                                    <ul class="dropdown-menu">{b}</ul>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="./contact" class="nav-link text-truncate mb-1">Contact Us</Link>
-                            </li>
-                        </ul>
-
-                    </div>
+                    <button class="navbar-toggler" type="button" onClick={() => {
+                        document.getElementsByClassName("phone_menu")[0].classList.toggle("display");
+                    }}>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div className="navlinks">
                         <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
                             <li class="nav-item mb-1">
@@ -143,6 +113,34 @@ export default function Navbar() {
                             </li>
                         </ul>
                     </div>
+                </div>
+                <div className="phone_menu">
+                    <ul class="navbar me-auto mb-0 mb-lg-0 pe-md-2 pe-lg-3">
+                        <li class="nav-item mb-1">
+                            <Link class="nav-link active text-truncate" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li class="nav-item mb-1">
+                            <Link to="./About" class="nav-link text-truncate">About Us</Link>
+                        </li>
+                        <li class="nav-item dropdown mb-1">
+                            <div class="btn-group dropnavlink">
+                                <Link to="./DTHrig" class="nav-link text-truncate">DTH Rig</Link>
+                                <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                <ul class="dropdown-menu">{a}</ul>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown mb-1">
+                            <div class="btn-group dropnavlink">
+                                <Link to="./ProductPart" class="nav-link text-truncate">Product Part</Link>
+                                <button type="button" class="btn btn-warning btn-sm text-black dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                <ul class="dropdown-menu">{b}</ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="./contact" class="nav-link text-truncate mb-1">Contact Us</Link>
+                        </li>
+                    </ul>
+
                 </div>
                 {showModel && <Mymodal />}
             </nav >
